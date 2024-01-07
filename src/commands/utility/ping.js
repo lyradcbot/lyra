1f679b79d677fb79cc7b9a904edcbfa7e77589d0
André Lucas <2007andrepaiva@gmail.com>
2024-01-07 14:52:42 -0300

    Add db ping

--- src/commands/utility/ping.js
+++ src/commands/utility/ping.js
@@ -1,11 +1,19 @@
 const { SlashCommandBuilder } = require('discord.js');
+const { connect, connection: db } = require('mongoose');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('ping')
 		.setDescription('Replies with Pong!'),
 	async execute(interaction) {
-		await interaction.reply(`:ping_pong: Pong!\n**API Ping:** \`${interaction.client.ws.ping}ms\`\n**WebSocket Ping:** \`${Date.now() - interaction.createdTimestamp}ms\``);
-
+		const databasePing = async () => {
+			const cNano = process.hrtime();
+			await db.db.command({
+				ping: 1,
+			});
+			const time = process.hrtime(cNano);
+			return (time[0] * 1e9 + time[1]) * 1e-6;
+		};
+		await interaction.reply(`:ping_pong: Pong!\n> **API Ping:** \`${interaction.client.ws.ping}ms\`\n> **WebSocket Ping:** \`${Date.now() - interaction.createdTimestamp}ms\`\n> **Database Ping:** \`${Math.round(await databasePing())}ms\``);
 	},
 };
