e811fbe45e97dc80e0d4b9605b6d4a5191db4f88
André Lucas <2007andrepaiva@gmail.com>
2024-01-07 13:53:59 -0300

    Feito a base do bot

---
+++ src/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+	client: {
+		id: process.env.CLIENT_ID,
+		secret: process.env.CLIENT_SECRET,
+		token: process.env.CLIENT_TOKEN,
+	},
+	database: {
+		uri: process.env.MONG_URI,
+	},
+};
---
+++ src/deployCommands.js
@@ -0,0 +1,40 @@
+const { REST, Routes } = require('discord.js');
+const config = require('./config.js');
+const fs = require('node:fs');
+const path = require('node:path');
+
+const commands = [];
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		if ('data' in command && 'execute' in command) {
+			commands.push(command.data.toJSON());
+		}
+		else {
+			console.log(`[WARNING] O comando no arquivo ${filePath} está faltado as propiedades "data" ou "execute".`.red);
+		}
+	}
+}
+
+const rest = new REST().setToken(config.client.token);
+
+(async () => {
+	try {
+		console.log(`[SLASH] Recarregando ${commands.length} slash commands.`.cyan);
+		const data = await rest.put(
+			Routes.applicationCommands(config.client.id),
+			{ body: commands },
+		);
+
+		console.log(`[SLASH] Recarregados ${data.length} slash commands.`.green);
+	}
+	catch (error) {
+		console.error(error);
+	}
+})();
