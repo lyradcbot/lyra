c3ff5e18af93738579de7538ee3e0b016d996d1d
André Lucas <2007andrepaiva@gmail.com>
2024-01-08 11:03:35 -0300

    GuildSchema

---
+++ src/schemas/guildSchema.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const ObjectId = Schema.ObjectId;
+
+const GuildSchema = new Schema({
+	_id: ObjectId,
+	guildId: String,
+	blocked: Boolean,
+	logsChannel: String,
+	autoThread: {
+		channels: Array,
+		enabled: Boolean,
+	},
+	autorole: {
+		users: Array,
+		bots: Array,
+		stickyRoles: Array,
+		enabled: Boolean,
+	},
+	suggestions: {
+		channel: String,
+		suggestions: Array,
+		enabled: Boolean,
+	},
+	tickets: {
+		transcriptChannel: String,
+		ticketCategory: String,
+		ticketChannel: String,
+		ticketEmbed: {
+			title: String,
+			description: String,
+			color: String,
+			footer: String,
+			thumbnail: String,
+			image: String,
+		},
+		enabled: Boolean,
+	},
+	premium: {
+		enabled: Boolean,
+		expires: Date,
+	},
+});
+
+module.exports = mongoose.model('Guild', GuildSchema);
