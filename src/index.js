bda40cde66bb73bd32362489c6ef1c15c6d9be86
eiandremoreira <ninfetahot123@gmail.com>
2024-01-08 17:32:23 -0300

    eventos de error

--- src/index.js
+++ src/index.js
@@ -43,6 +43,12 @@ for (const file of eventFiles) {
 	}
 }
 
+process.on('unhandledRejection', (err) => console.log(err));
+
+process.on('uncaughtException', (err) => console.log(err));
+
+client.on('error', (err) => console.log(err));
+
 mongoose.connect(config.database.uri)
 	.then(() => {
 		console.log('[DATABASE] Database Ready!'.green);
