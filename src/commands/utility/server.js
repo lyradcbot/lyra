2186c76cca69570d95c41f99fb85ad581fce4f38
eiandremoreira <ninfetahot123@gmail.com>
2024-01-08 20:29:02 -0300

    servericon fim

--- src/commands/utility/server.js
+++ src/commands/utility/server.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, StringSelectMenuBuilder, ComponentType } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, StringSelectMenuBuilder } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -40,7 +40,7 @@ module.exports = {
 			if (!guild.icon) return interaction.reply('Esse servidor não tem um icone');
 
 			const embed = new EmbedBuilder()
-				.setTitle(`Icone de \`${guild.name}\``)
+				.setTitle(`Ícone de \`${guild.name}\``)
 				.setImage(guild.iconURL({ dynamic: true, size: 2048 }))
 				// .setTimestamp()
 				// .setColor(color)
@@ -76,22 +76,7 @@ module.exports = {
 					.addComponents(menu);
 
 
-				const response = interaction.followUp({ content: 'Achei que talvez você ficaria curioso de ver o ícone desses outros servidores também:', components: [row], ephemeral: true });
-
-				const collector = response.createMessageComponentCollector({ componentType: ComponentType.StringSelect, time: 60000 });
-
-				collector.on('collect', async i => {
-					guild = interaction.guilds.cache.get(i.values[0]);
-					const embe = new EmbedBuilder()
-						.setTitle(`Icone de \`${guild.name}\``)
-						.setImage(guild.iconURL({ dynamic: true, size: 2048 }))
-					// .setTimestamp()
-					// .setColor(color)
-						.setFooter({ text: `${interaction.user.tag} (${interaction.user.id})` });
-
-					await i.reply({ embeds: [embe] });
-				});
-
+				interaction.followUp({ content: 'Achei que talvez você ficaria curioso de ver o ícone desses outros servidores também:', components: [row], ephemeral: true });
 
 	   }, 5000);
 
---
+++ src/events/interactionMenus.js
@@ -0,0 +1,23 @@
+const { Events, EmbedBuilder, } = require('discord.js');
+module.exports = {
+	name: Events.InteractionCreate,
+	async execute (interaction) {
+		if (!interaction.isUserContextMenuCommand()) return;
+
+		if(interaction.customId == '') {
+			const guild = interaction.client.guilds.cache.get(interaction.values.join(''));
+
+			if(!guild.icon) return interaction.reply('Esse servidor não tem um icone');
+
+			const embed = new EmbedBuilder()
+				.setTitle(`Ícone de \`${guild.name}\``)
+				.setImage(guild.iconURL({ dynamic: true, size: 2048 }))
+				.setTimestamp()
+				.setColor('#f283cc')
+				.setFooter({ text: `${interaction.user.tag} (${interaction.user.id})` });
+
+
+			interaction.reply({ content: interaction.user.toString(), embeds: [embed], ephemeral: true });
+		}
+	}
+};
