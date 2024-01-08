4ff406c01ebcf03ff0991528d305799f262b07e0
eiandremoreira <ninfetahot123@gmail.com>
2024-01-08 11:05:26 -0300

    flags emoji

--- src/commands/utility/user.js
+++ src/commands/utility/user.js
@@ -25,7 +25,7 @@ module.exports = {
 				flags.push('BOT');
 			}
 			flags = flags.join('');
-			flags = flags.replace(/DISCORD_EMPLOYEE/g, '<:staff_dc:927229606124191754>').replace(/HYPESQUAD_EVENTS/g, '<:hypersquad:927229968176525382>').replace(/HOUSE_BRILLIANCE/g, '<:hps_brillinance:927230429805834260>').replace(/HOUSE_BRAVERY/g, '<:bravery:927236752643600404>').replace(/HOUSE_BALANCE/g, '<:balance:927236471017058365>').replace(/EARLY_SUPPORTER/g, '<:porcokkkkk:927230734391967754>').replace(/BUGHUNTER_LEVEL_2/g, '<:bug_lvl2:927230992593334292>').replace(/EARLY_VERIFIED_BOT_DEVELOPER/g, '<:bot_developer:927231423063150622>').replace(/DISCORD_CERTIFIED_MODERATOR/g, '<:moderator:927232655836184626>').replace(/PARTNERED_SERVER_OWNER/g, '<:partner:927243125741719573>').replace(/BUGHUNTER_LEVEL_1/g, '<:bug:927244179128602625>').replace(/VERIFIED_BOT/g, '<:botv:927248783576817674>').replace(/BOT/g, '<:bot:927249890503950336>');
+			flags = flags.replace(/Staff/g, '<:staff_dc:927229606124191754>').replace(/Hypesquad/g, '<:hypersquad:927229968176525382>').replace(/HypeSquadOnlineHouse2/g, '<:hps_brillinance:927230429805834260>').replace(/HypeSquadOnlineHouse1/g, '<:bravery:927236752643600404>').replace(/HypeSquadOnlineHouse3/g, '<:balance:927236471017058365>').replace(/PremiumEarlySupporter/g, '<:porcokkkkk:927230734391967754>').replace(/BugHunterLevel2/g, '<:bug_lvl2:927230992593334292>').replace(/ActiveDeveloper/g, '<:bot_developer:927231423063150622>').replace(/CertifiedModerator/g, '<:moderator:927232655836184626>').replace(/Partner/g, '<:partner:927243125741719573>').replace(/BugHunterLevel1/g, '<:bug:927244179128602625>').replace(/VerifiedBot/g, '<:botv:927248783576817674>').replace(/BOT/g, '<:bot:927249890503950336>');
 			return flags;
 		}
 
