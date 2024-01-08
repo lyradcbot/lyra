d5634d3533e790d38901b53939a312b892e37354
André Lucas <2007andrepaiva@gmail.com>
2024-01-08 17:12:26 -0300

    Fix auto Threads

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -4,12 +4,15 @@ const emoji = require('../modules/emojis.json');
 module.exports = {
 	name: Events.MessageCreate,
 	async execute (message) {
-		if (message.author.bot) return;
 		if (message.channel.type === ChannelType.DM) return;
 		const autoThreadEnabled = await message.client.db.guild.getAutoThreadStatus(message.guild.id);
 		if (autoThreadEnabled) {
 			const autoThreadChannels = await message.client.db.guild.getAutoThreadChannels(message.guild.id);
 			if (!autoThreadChannels.includes(message.channel.id)) return;
+			let startMessage = message.content;
+			if (startMessage.length === 0) {
+				startMessage = 'Thread criada automaticamente';
+			}
 			const thread = await message.startThread({
 				name: message.content.slice(0, 10) + '...',
 				autoArchiveDuration: 1440,
@@ -33,5 +36,6 @@ module.exports = {
 				components: [row],
 			});
 		}
+		// TODO: Caso for fazer algo, o autoThread é o único sistema disponível para bots if (message.author.bot) return;
 	},
 };
