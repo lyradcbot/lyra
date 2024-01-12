7fdee7722fa29e1155736b0012d4aafa890dcad7
André Lucas <2007andrepaiva@gmail.com>
2024-01-12 17:56:52 -0300

    Fix on ready

--- src/events/ready.js
+++ src/events/ready.js
@@ -7,27 +7,29 @@ module.exports = {
 	async execute (client) {
 		console.log(`[CLIENT] Pronto! Estou ligado em ${client.user.tag}`.green);
 		await client.vulkava.start(client.user.id);
-		client.guilds.cache.map(async (guild) => {
-			await client.db.lavalink.checkPlayer(guild.id);
-			const data = await client.db.lavalink.getPlayer(guild.id);
-			if (data.tracks.length > 0) {
-				const player = client.vulkava.createPlayer({
-					guildId: guild.id,
-					voiceChannelId: data.voiceChannelId,
-					textChannelId: data.textChannelId,
-					selfDeaf: true
-				});
-				data.tracks.map(track => {
-					player.queue.add(track);
-				});
-				player.connect();
-				if (!player.playing) player.play();
-				const channel = await client.channels.cache.get(data.textChannelId);
-				const returnEmbed = new EmbedBuilder()
-					.setDescription(`${emoji.piscada} **|** Eu havia reiniciado, mas já estou retomando a música!`)
-					.setColor('Blurple');
-				await channel.send({ embeds: [returnEmbed] });
-			}
-		});
+		setTimeout(() => {
+			client.guilds.cache.map(async (guild) => {
+				await client.db.lavalink.checkPlayer(guild.id);
+				const data = await client.db.lavalink.getPlayer(guild.id);
+				if (data.tracks.length > 0) {
+					const player = client.vulkava.createPlayer({
+						guildId: guild.id,
+						voiceChannelId: data.voiceChannelId,
+						textChannelId: data.textChannelId,
+						selfDeaf: true
+					});
+					data.tracks.map(track => {
+						player.queue.add(track);
+					});
+					player.connect();
+					if (!player.playing) player.play();
+					const channel = await client.channels.cache.get(data.textChannelId);
+					const returnEmbed = new EmbedBuilder()
+						.setDescription(`${emoji.piscada} **|** Eu havia reiniciado, mas já estou retomando a música!`)
+						.setColor('Blurple');
+					await channel.send({ embeds: [returnEmbed] });
+				}
+			});
+		}, 10000);
 	},
 };
