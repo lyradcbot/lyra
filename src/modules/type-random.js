48999687679301030bfbba5712f0b598049fa942
eiandremoreira <ninfetahot123@gmail.com>
2024-02-16 17:52:47 -0300

    fix bug

--- src/modules/type-random.js
+++ src/modules/type-random.js
@@ -1,13 +1,13 @@
-const getRandomNumber = (length: number) => Math.floor(Math.random() * length)
+const getRandomNumber = (length) => Math.floor(Math.random() * length);
 
-const getRandomWords = (words: string[]) => {
-  const response = []
-  
-  for (let i = 0; i <= words.length; i++) {
-    response.push(words[getRandomNumber(words.length)])
-  }
+const text = (words) => {
+	const response = [];
 
-  return response
-}
+	for (let i = 0; i <= words.length; i++) {
+		response.push(words[getRandomNumber(words.length)]);
+	}
+
+	return response;
+};
 
 module.exports = text;
